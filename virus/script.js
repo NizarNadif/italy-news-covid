dd00a797447f424080ffc7a774c079e6e05593a1
Nizar Nadif <56386611+NizarNadif@users.noreply.github.com>
2020-12-07 21:23:03 +0100

    map finished

--- virus/script.js
+++ virus/script.js
@@ -2,11 +2,11 @@ const covid_regioni = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master
 const covid_province = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province-latest.json";
 
 /* Layers */
-const layer_basico = new L.TileLayer('https://api.mapbox.com/styles/v1/sbibbof/ckhjt37bw4p1a19kzdtzhnr3e/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JpYmJvZiIsImEiOiJja2dyMHBqaGswOWR2MnpvN3E4OWFpbGNuIn0.qAXx979h5T2-NOhIv3sCRg');
-const layer_rilievi = new L.TileLayer('https://api.mapbox.com/styles/v1/sbibbof/ckgr0u13m0xj619opait0jcv1/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JpYmJvZiIsImEiOiJja2dyMHBqaGswOWR2MnpvN3E4OWFpbGNuIn0.qAXx979h5T2-NOhIv3sCRg');
-const layer_strade = new L.TileLayer('https://api.mapbox.com/styles/v1/sbibbof/ckhjstoc84omt19qctz0ev1en/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JpYmJvZiIsImEiOiJja2dyMHBqaGswOWR2MnpvN3E4OWFpbGNuIn0.qAXx979h5T2-NOhIv3sCRg');
-const layer_scuro = new L.TileLayer('https://api.mapbox.com/styles/v1/sbibbof/ckhjt6ik14p1h19np4fcgdzg1/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JpYmJvZiIsImEiOiJja2dyMHBqaGswOWR2MnpvN3E4OWFpbGNuIn0.qAXx979h5T2-NOhIv3sCRg');
-const layer_group = [layer_basico, layer_strade, layer_scuro, layer_rilievi];
+const layer_basico = new L.TileLayer('https://api.mapbox.com/styles/v1/sbibbof/ckhjt37bw4p1a19kzdtzhnr3e/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JpYmJvZiIsImEiOiJja2dyMHBqaGswOWR2MnpvN3E4OWFpbGNuIn0.qAXx979h5T2-NOhIv3sCRg', {noWrap: true});
+const layer_rilievi = new L.TileLayer('https://api.mapbox.com/styles/v1/sbibbof/ckgr0u13m0xj619opait0jcv1/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JpYmJvZiIsImEiOiJja2dyMHBqaGswOWR2MnpvN3E4OWFpbGNuIn0.qAXx979h5T2-NOhIv3sCRg', {noWrap: true});
+const layer_strade = new L.TileLayer('https://api.mapbox.com/styles/v1/sbibbof/ckhjstoc84omt19qctz0ev1en/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JpYmJvZiIsImEiOiJja2dyMHBqaGswOWR2MnpvN3E4OWFpbGNuIn0.qAXx979h5T2-NOhIv3sCRg', {noWrap: true});
+const layer_scuro = new L.TileLayer('https://api.mapbox.com/styles/v1/sbibbof/ckhjt6ik14p1h19np4fcgdzg1/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2JpYmJvZiIsImEiOiJja2dyMHBqaGswOWR2MnpvN3E4OWFpbGNuIn0.qAXx979h5T2-NOhIv3sCRg', {noWrap: true});
+const layer_group = [layer_rilievi, layer_strade, layer_scuro, layer_basico];
 
 const capitali = "https://gist.githubusercontent.com/erdem/8c7d26765831d0f9a8c62f02782ae00d/raw/248037cd701af0a4957cce340dabb0fd04e38f4c/countries.json";
 const covid_mondo = "https://api.covid19api.com/summary";
@@ -29,11 +29,11 @@ let stato_mappa = {
     container: 'map',
     center: new L.LatLng(posRoma.lat, posRoma.long),
     zoom: 6, 
-    /* minZoom: map_zoom,
+    minZoom: 2,
     maxBounds: [
-        [47.4979, 19.0402],
-        [36.1898, 5.4108]
-    ], */
+        [85, -177.4],
+        [-85, 177.4]
+    ],
     layers: layer_group
 }
 
@@ -56,14 +56,14 @@ async function main() {
     /* imposto la mappa in modo da poter cambiare modalità */
     zoomControl(mappa);
     const baseMaps = {
+        "Rilievi": layer_rilievi,
         "Strade": layer_strade,
         "Scuro": layer_scuro,
-        "Rilievi": layer_rilievi,
         "Basico": layer_basico
     };
     L.control.layers(baseMaps).addTo(mappa);
-    
-    draw(regioni);
+
+    drawItaly(regioni);
 }
 this.main();
 
@@ -101,19 +101,13 @@ function error() {
 
 function zoomControl(map) {
     map.on('zoomend', function (event) {
-        /*
-        5 -> stati
-        6 -> regioni
-        7 -> province
-        */
-        console.log(map.getZoom());
         if (mappa.getZoom() <= 5) drawNations(stati);
-        else if (mappa.getZoom() <= 7) draw(regioni);
-        else draw(province);
+        else if (mappa.getZoom() <= 7) drawItaly(regioni);
+        else drawItaly(province);
     });
 }
 
-function draw(data) {
+function drawItaly(data) {
     let modRegioni = data[0].denominazione_provincia == null;
 
     removeOverlays(mappa, overlays);
@@ -128,20 +122,16 @@ function draw(data) {
             if (luogo.totale_casi >= 20000) {
                 if (modRegioni) diametro = 5;
                 else diametro = 4;
-                bordo = 'red';
                 fill = 'red';
             } else if (luogo.totale_casi >= 15000) {
                 if (modRegioni) diametro = 4;
                 else diametro = 3.5;
-                bordo = 'orange';
                 fill = 'orange';
             } else if (luogo.totale_casi >= 10000) {
                 diametro = 3;
-                bordo = 'orange';
-                fill = 'orange';
+                fill = 'coral';
             } else {
                 diametro = 1.5;
-                bordo = 'yellow';
                 fill = 'yellow';
             }
             if (modRegioni) diametro *= 10000;
@@ -149,9 +139,8 @@ function draw(data) {
 
             //creo il cerchio e lo aggiungo nella mappa
             const cerchio = L.circle([luogo.lat, luogo.long], diametro, {
-                color: bordo,
-                fillColor: fill,
-                fillOpacity: 0.6
+                color: fill,
+                fillOpacity: 0.4
             }).addTo(mappa);
 
             overlays.push(cerchio);
@@ -289,20 +278,30 @@ function drawNations(data) {
     let iteratore = data.values();
     data.forEach(stato => {
         if (stato.Country == undefined) return;
-        const diametro = 20000;
-        const punto = L.circle([stato.lat, stato.long], diametro);
+        let dati_cerchio = { radius: 60000, color: 'green' };
+        if (stato.TotalConfirmed >= 10000000) dati_cerchio = { radius: dati_cerchio.radius * 20, color: 'red' }
+        else if (stato.TotalConfirmed >= 5000000) dati_cerchio = { radius: dati_cerchio.radius * 10, color: 'red' }
+        else if (stato.TotalConfirmed >= 2500000) dati_cerchio = { radius: dati_cerchio.radius * 7, color: 'red' }
+        else if (stato.TotalConfirmed >= 1500000) dati_cerchio = { radius: dati_cerchio.radius * 6, color: 'red' }
+        else if (stato.TotalConfirmed >= 1000000) dati_cerchio = { radius: dati_cerchio.radius * 5, color: 'orange' }
+        else if (stato.TotalConfirmed >= 500000) dati_cerchio = { radius: dati_cerchio.radius * 4, color: 'orange' }
+        else if (stato.TotalConfirmed >= 250000) dati_cerchio = { radius: dati_cerchio.radius * 2, color: 'orange' }
+        else if (stato.TotalConfirmed >= 100000) dati_cerchio = { radius: dati_cerchio.radius * 1.5, color: 'coral' }
+        const punto = L.circle([stato.lat, stato.long], dati_cerchio.radius, {
+            color: dati_cerchio.color,
+            fillOpacity: 0.4
+        });
         punto.addTo(mappa);
         overlays.push(punto);
 
         punto.on("click", function () {
-            console.log(stato.lat + ' ' + stato.long)
-
+            
             //creo il popup e lo aggiungo alla mappa
-             const popup = L.popup()
-                 .setLatLng(L.latLng(stato.lat, stato.long))
-                 .setContent('<p><b>' +
+            const popup = L.popup()
+                .setLatLng(L.latLng(stato.lat, stato.long))
+                .setContent('<p><b>' +
                     '<img class="bandiera" src="https://raw.githubusercontent.com/hjnilsson/country-flags/master/png100px/' + stato.CountryCode.toLowerCase() + '.png">' +
-                 stato.Country + '</b><br> Casi: ' + stato.TotalConfirmed.toLocaleString() + ' <span style="color: green"> ( +' + stato.NewConfirmed.toLocaleString() + ') </span></p>')
+                stato.Country + '</b><br> Casi: ' + stato.TotalConfirmed.toLocaleString() + ' <span style="color: green"> ( +' + stato.NewConfirmed.toLocaleString() + ') </span></p>')
                 .openOn(mappa);
             
             let tabella_body = createTable(stato.Country + ' (' + stato.CountryCode + ')', null, stato.Date)
